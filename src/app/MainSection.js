60c84254746eadeca842e6071476bae773a1c8be
Austin Ohlfs <ohlfsam96@gmail.com>
2024-05-27 18:37:21 -0600

    toatsTest2

--- src/app/MainSection.js
+++ src/app/MainSection.js
@@ -20,7 +20,7 @@ const MainSection = ({ isMobile }) => {
               <div className="container" style={styles.container}>
                 <section id="introduction">
                   <div className="container" style={styles.introContainer}>
-                    <h2 style={styles.heading}>Introduction</h2>
+                    <h2 style={styles.heading}>Introductions</h2>
                     <p style={styles.paragraph}>Hello and welcome to my corner of the digital universe! I'm Austin, a Colorado native with a passion for both coding and carving through fresh powder on the slopes. With over a decade of experience honing skis and snowboards in the picturesque town of Vail, I've learned the importance of precision, attention to detail, and the thrill of mastering a craft.</p>
                     <p style={styles.paragraph}>Transitioning from mountains to code, I bring that same dedication and expertise to my work as a full-stack software engineer. Specializing in crafting dynamic webpages and software solutions, I thrive on leveraging technology to drive business growth and efficiency.</p>
                     <p style={styles.paragraph}>Here, you'll find a showcase of my projects, each meticulously designed to not only meet but exceed client expectations. From responsive web designs to scalable software applications, I'm committed to delivering solutions that empower businesses to thrive in the digital landscape.</p>
