c68b2f740e7de9267ce853c871a7176e4f5a598f
Austin Ohlfs <ohlfsam96@gmail.com>
2024-05-28 18:34:28 -0600

    SEO

--- src/app/IntroSection.js
+++ src/app/IntroSection.js
@@ -1,24 +1,62 @@
 import React from 'react';
-import { Container, Header, Segment } from 'semantic-ui-react';
+import { Container, Header, Segment, Icon } from 'semantic-ui-react';
+import { motion } from 'framer-motion';
 import Skills from './Skills'; // Adjust the import according to your project structure
 
 const IntroductionSection = ({ isMobile }) => {
   return (
     <Segment inverted vertical style={styles.segment}>
       <Container text>
-        <Header as='h2' inverted style={styles.heading}>Introduction</Header>
-        <p style={styles.paragraph}>
-          Hello and welcome to my corner of the digital universe! I'm Austin, a Colorado native with a passion for both coding and carving through fresh powder on the slopes. With over a decade of experience honing skis and snowboards in the picturesque town of Vail, I've learned the importance of precision, attention to detail, and the thrill of mastering a craft.
-        </p>
-        <p style={styles.paragraph}>
-          Transitioning from mountains to code, I bring that same dedication and expertise to my work as a full-stack software engineer. Specializing in crafting dynamic webpages and software solutions, I thrive on leveraging technology to drive business growth and efficiency.
-        </p>
-        <p style={styles.paragraph}>
-          Here, you'll find a showcase of my projects, each meticulously designed to not only meet but exceed client expectations. From responsive web designs to scalable software applications, I'm committed to delivering solutions that empower businesses to thrive in the digital landscape.
-        </p>
-        <p style={styles.paragraph}>
-          So, whether you're navigating the slopes or navigating the web, let's embark on a journey of innovation together. Let's harness the power of technology to elevate your business to new heights.
-        </p>
+        <motion.div
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <Header as='h2' inverted style={styles.heading}>
+            <Icon name='code' style={styles.icon} />
+            Welcome!
+          </Header>
+          <motion.p
+            style={styles.paragraph}
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+          >
+            Hello and welcome to my corner of the digital universe! I'm Austin, a Colorado native with a passion for both coding and carving through fresh powder on the slopes.
+          </motion.p>
+          <motion.p
+            style={styles.paragraph}
+            initial={{ opacity: 0, x: 50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: 0.4, duration: 0.5 }}
+          >
+            With over a decade of experience honing skis and snowboards in the picturesque town of Vail, I've learned the importance of precision, attention to detail, and the thrill of mastering a craft.
+          </motion.p>
+          <motion.p
+            style={styles.paragraph}
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: 0.6, duration: 0.5 }}
+          >
+            Transitioning from mountains to code, I bring that same dedication and expertise to my work as a full-stack software engineer. Specializing in crafting dynamic webpages and software solutions, I thrive on leveraging technology to drive business growth and efficiency.
+          </motion.p>
+          <motion.p
+            style={styles.paragraph}
+            initial={{ opacity: 0, x: 50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: 0.8, duration: 0.5 }}
+          >
+            Here, you'll find a showcase of my projects, each meticulously designed to not only meet but exceed client expectations. From responsive web designs to scalable software applications, I'm committed to delivering solutions that empower businesses to thrive in the digital landscape.
+          </motion.p>
+          <motion.p
+            style={styles.paragraph}
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 1, duration: 0.5 }}
+          >
+            So, whether you're navigating the slopes or navigating the web, let's embark on a journey of innovation together. Let's harness the power of technology to elevate your business to new heights.
+          </motion.p>
+        </motion.div>
         {isMobile && <Skills isMobile={true} />}
       </Container>
     </Segment>
@@ -27,21 +65,30 @@ const IntroductionSection = ({ isMobile }) => {
 
 const styles = {
   segment: {
-    padding: '4em 0em',
+    padding: '4em 2em',
     background: 'linear-gradient(to right, #4A90E2, #58B3D3)',
     borderRadius: '20px',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
   },
   heading: {
     fontSize: '2.5em',
     fontWeight: 'bold',
     marginBottom: '0.5em',
     color: '#fff',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  icon: {
+    marginRight: '0.5em',
+    color: '#fff',
   },
   paragraph: {
     fontSize: '1.2em',
     lineHeight: '1.5',
     color: '#fff',
+    marginBottom: '1em',
   },
 };
 
 export default IntroductionSection;
+
--- src/app/WorkExp.js
+++ src/app/WorkExp.js
@@ -12,12 +12,100 @@ const WorkExp = () => {
         <div className="container">
           <StyledHeader>Work Experience</StyledHeader>
           <JobExperience>
-            <JobTitle>Charter Sports - Tune Shop Manager</JobTitle>
-            <StyledImage src="https://thecharter.com/wp-content/uploads/2020/12/Charter-Sports-logo.jpg" size="small" />
+
+          <JobTitle>ÆSIR SPEED TECHS -Software Engineer/Wax Tech</JobTitle>
+<StyledImage src="/AESIRLogo.png" size="small" style={{background: 'white'}}/>
+<StyledList>
+  <li><strong>Overview:</strong> Served as the Lead Software Engineer and Wax Technician, responsible for building in-house software, booking systems, and designing the company website, while also maintaining professional athletes' equipment to the highest standards.</li>
+  
+  <li><strong>Key Responsibilities:</strong>
+    <ul>
+      <li>Developed and implemented comprehensive in-house software solutions, enhancing operational efficiency and accuracy.</li>
+      <li>Designed and built a robust booking system to streamline client reservations and management processes.</li>
+      <li>Created and maintained the company website, ensuring a user-friendly experience and high performance.</li>
+      <li>Performed detailed tuning and maintenance on professional ski and snowboard equipment, ensuring top performance for athletes.</li>
+      <li>Traveled with professional athletes to provide on-site technical support and equipment maintenance during competitions.</li>
+    </ul>
+  </li>
+  
+  <li><strong>Skills Gained:</strong>
+    <ul>
+      <li>Advanced proficiency in full-stack development, with expertise in both front-end and back-end technologies.</li>
+      <li>Expertise in designing and implementing responsive, high-performance web applications.</li>
+      <li>In-depth knowledge of ski and snowboard equipment maintenance and tuning.</li>
+      <li>Ability to provide technical support and maintain high standards under competitive conditions.</li>
+    </ul>
+  </li>
+  
+  <li><strong>Additional Highlights:</strong>
+    <ul>
+      <li>Reduced operational downtime by automating key processes and implementing efficient software solutions.</li>
+      <li>Improved customer satisfaction through the development of a seamless booking experience and high-quality website design.</li>
+      <li>Enhanced athletes' performance by ensuring equipment was maintained to the highest standards at all times.</li>
+      <li>Recognized for exceptional technical skills and dedication to supporting professional athletes during high-stakes competitions.</li>
+    </ul>
+  </li>
+</StyledList>
+
+          <JobTitle>FlatIron School - Student</JobTitle>
+            <StyledImage src="https://upload.wikimedia.org/wikipedia/commons/6/61/FS_wiki.png" size="small" style={{background: 'white'}}/>
             <StyledList>
-              <li>Trained and managed staff to maintain rental and customer equipment (skis/snowboards/bikes)</li>
-              <li>Ran and maintained shop machines and equipment</li>
+              <li><strong>Overview:</strong> An intensive 5-month program focused on full-stack web development, covering both front-end and back-end technologies. The curriculum was divided into multiple 3-week modules, each dedicated to mastering different technologies and frameworks.</li>
+              
+              <li><strong>Skills Gained:</strong>
+                <ul>
+                  <li>Proficient in full-stack development with hands-on experience in multiple frameworks and libraries.</li>
+                  <li>Strong understanding of software development lifecycle, including Agile methodologies.</li>
+                  <li>Ability to implement responsive and dynamic web applications.</li>
+                  <li>Experience with version control and collaborative development workflows.</li>
+                  <li>Knowledge of testing frameworks and best practices for ensuring code quality.</li>
+                </ul>
+              </li>
+              
+              <li><strong>Additional Highlights:</strong>
+                <ul>
+                  <li>Participated in daily stand-ups, code reviews, and pair programming sessions.</li>
+                  <li>Engaged in continuous learning and improvement through coding challenges and hackathons.</li>
+                  <li>Developed soft skills such as problem-solving, communication, and teamwork.</li>
+                </ul>
+              </li>
             </StyledList>
+
+
+            <JobTitle>Charter Sports - Tune Shop Manager</JobTitle>
+<StyledImage src="https://thecharter.com/wp-content/uploads/2020/12/Charter-Sports-logo.jpg" size="small" />
+<StyledList>
+  <li><strong>Overview:</strong> Managed the operations of the tune shop and rental shop, ensuring high-quality maintenance of customer and rental equipment, while leading a team to achieve efficiency and cost savings for the company.</li>
+
+  <li><strong>Key Responsibilities:</strong>
+    <ul>
+      <li>Ran and maintained shop machines and equipment, ensuring they operated at peak performance.</li>
+      <li>Trained and supervised staff to maintain and repair rental and customer equipment (skis, snowboards, bikes).</li>
+      <li>Implemented efficient workflows and quality control measures to ensure high standards of service and maintenance.</li>
+      <li>Oversaw inventory management, reducing waste and optimizing resource usage.</li>
+      <li>Provided excellent customer service, addressing and resolving customer issues promptly and professionally.</li>
+    </ul>
+  </li>
+
+  <li><strong>Skills Gained:</strong>
+    <ul>
+      <li>Leadership and team management: Effectively led and motivated a team to achieve high performance and quality standards.</li>
+      <li>Technical expertise: Developed in-depth knowledge of tuning and maintaining a variety of sports equipment.</li>
+      <li>Operational efficiency: Implemented processes that improved shop efficiency and reduced operational costs.</li>
+      <li>Customer service: Enhanced customer satisfaction through excellent service and effective problem-solving.</li>
+      <li>Inventory management: Optimized inventory levels, reducing waste and ensuring timely availability of supplies.</li>
+    </ul>
+  </li>
+
+  <li><strong>Additional Highlights:</strong>
+    <ul>
+      <li>Reduced operational costs by implementing efficient maintenance procedures and reducing equipment downtime.</li>
+      <li>Developed and conducted training programs for new employees, ensuring they were proficient in equipment maintenance and customer service.</li>
+      <li>Maintained high standards of safety and quality, contributing to the company's reputation for reliable service.</li>
+      <li>Recognized for consistently meeting and exceeding performance targets, resulting in increased customer satisfaction and repeat business.</li>
+    </ul>
+  </li>
+</StyledList>
 
             <JobTitle>Ski Base - Tuner</JobTitle>
             <StyledImage src="https://vailskibase.com/wp-content/uploads/2021/09/ski-base-logo-lg-1.jpg" size="small" />
